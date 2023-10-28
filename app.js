c2edfd5e2dbc40f98f777fc681ea15d4394b65bd
AlexMaAU <alexma8809@gmail.com>
2023-10-28 21:53:17 +1100

    db added

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-const express = require('express')
-require('express-async-errors')  //这个包一定要在导入express后再导入，否则不起作用，专门用来处理异步错误。但是还是需要自定义错误中间件来处理要自定义的错误处理。
-const app = express()
-require('dotenv').config()
-const indexRouter = require('./routes/index')
-const connectDB = require('./utils/db')
-const unknownError = require('./middlewares/unknownError')
-const validateError = require('./middlewares/validateError')
+const express = require('express');
+require('express-async-errors'); //这个包一定要在导入express后再导入，否则不起作用，专门用来处理异步错误。但是还是需要自定义错误中间件来处理要自定义的错误处理。
+const app = express();
+require('dotenv').config();
+const indexRouter = require('./routes/index');
+const connectDB = require('./utils/db');
+const unknownError = require('./middlewares/unknownError');
+const validateError = require('./middlewares/validateError');
 
-const PORT = process.env.PORT||3000
+const PORT = process.env.PORT || 3000;
 
-app.use(indexRouter)
+app.use(indexRouter);
 
-app.use(validateError)  //其他详细类型的错误处理中间件要在未知错误处理中间件的前面
+app.use(validateError); //其他详细类型的错误处理中间件要在未知错误处理中间件的前面
 
-app.use(unknownError)  //未知错误处理中间件要放在最后面
+app.use(unknownError); //未知错误处理中间件要放在最后面
 
-connectDB()
-.then(()=>{
-    app.listen(PORT,()=>{
-        console.log(`server running at http://127.0.0.1:${PORT}`)
-    })
-}
-)
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server running at http://127.0.0.1:${PORT}`);
+  });
+});
--- routes/students.js
+++ routes/students.js
@@ -1,27 +1,35 @@
-const express = require('express')
-const studentRouter = express.Router()
-const {getAllStudents,getStudentById,createStudent,updateStudent,deleteStudent,addStudentToCourse,removeStudentFromCourse} = require('../controllers/students')
+const express = require('express');
+const studentRouter = express.Router();
+const {
+  getAllStudents,
+  getStudentById,
+  createStudent,
+  updateStudent,
+  deleteStudent,
+  addStudentToCourse,
+  removeStudentFromCourse,
+} = require('../controllers/students');
 
 //get all students, can filter by query
 //不用自己写中间件，使用第三方包：express-async-errors
-studentRouter.get('/', getAllStudents)  //catchAll函数把routeHandler中间件进行try catch验证，如果有错误，把错误pass to error handling middleware
+studentRouter.get('/', getAllStudents); //catchAll函数把routeHandler中间件进行try catch验证，如果有错误，把错误pass to error handling middleware
 
 //get student by id
-studentRouter.get('/:id', getStudentById)  //catchAll函数把routeHandler中间件进行try catch验证，如果有错误，把错误pass to error handling middleware
- 
+studentRouter.get('/:id', getStudentById); //catchAll函数把routeHandler中间件进行try catch验证，如果有错误，把错误pass to error handling middleware
+
 //create new student
-studentRouter.post('/', createStudent)
+studentRouter.post('/', createStudent);
 
 //update student by id
-studentRouter.put('/', updateStudent)
+studentRouter.put('/', updateStudent);
 
 //delete student by id in body of request
-studentRouter.delete('/', deleteStudent)
+studentRouter.delete('/', deleteStudent);
 
 //add Student To Course
-studentRouter.post('/:studentId/courses/:courseId', addStudentToCourse)
+studentRouter.post('/:studentId/courses/:courseId', addStudentToCourse);
 
 //delete Student from Course
-studentRouter.delete('/:studentId/courses/:courseId', removeStudentFromCourse)
+studentRouter.delete('/:studentId/courses/:courseId', removeStudentFromCourse);
 
-module.exports = studentRouter
+module.exports = studentRouter;
